7b605064ba4c21097df4ae4e65fa7a8d749323cf
magicDGS <daniel.gomez.sanchez@hotmail.es>
2023-10-30 01:12:40 +0100

    Add Card schema with enums/helper-models and approval test

---
+++ model/src/main/java/io/github/ygojson/model/atomic/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains the atomic models.
+ * </br>
+ * Atomic models are those where the information
+ * is not related to other models and contain only
+ * the information that represents.
+ */
+package io.github.ygojson.model.atomic;
---
+++ model/src/main/java/io/github/ygojson/model/enums/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Contains the enums that are hardcoded into YGOJSON.
+ * </br>
+ * Enums are models that are defined within YGOJSON.
+ * Other models, as they depend on releases (i.e., monster types)
+ * by the game, are generated and thus are defined in the schemas
+ * as strings.
+ */
+package io.github.ygojson.model.enums;
