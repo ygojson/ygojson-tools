1738436ad097f5a3f39245b2de1e8bddd06a1470
Daniel Gómez-Sánchez <7352559+magicDGS@users.noreply.github.com>
2023-11-04 18:34:33 +0100

    Add Card model (#1)
    * Add first data model (Card) with the internal definitions
    * Add schema validation acceptance tests
      (to prevent changes to the model without realizing)
    * Misc.: fix some dependencies

---
+++ model/src/main/java/io/github/ygojson/model/data/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains the Data Models.
+ * </br>
+ * Data Models are those that exists independently
+ * and only related between them by IDs.
+ */
+package io.github.ygojson.model.data;
---
+++ model/src/test/java/io/github/ygojson/model/data/acceptance/PackageSettings.java
@@ -0,0 +1,9 @@
+package io.github.ygojson.model.data.acceptance;
+
+/**
+ * Settings for the approval tests configuration.
+ */
+public class PackageSettings {
+
+    public static String ApprovalBaseDirectory = "../resources";
+}
