a791a60d17a4010f02ff7eea5dc960e3c7a2252d
magicDGS <daniel.gomez.sanchez@hotmail.es>
2023-11-27 00:04:55 +0100

    Bootstrap cli with spring

--- tools/src/main/java/io/github/ygojson/tools/Application.java
+++ tools/src/main/java/io/github/ygojson/tools/Application.java
@@ -1,14 +1,58 @@
 package io.github.ygojson.tools;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import picocli.CommandLine;
 
+import io.github.ygojson.tools.cli.ErrorCodes;
+import io.github.ygojson.tools.cli.ParentCli;
+import io.github.ygojson.tools.common.YgoJsonTool;
+
+/**
+ * Entry point of the application.
+ */
 @Slf4j
 @SpringBootApplication
-public class Application {
+@ComponentScan(
+	includeFilters = {
+		@ComponentScan.Filter(
+			type = FilterType.ASSIGNABLE_TYPE,
+			value = { YgoJsonTool.class }
+		),
+	}
+)
+@RequiredArgsConstructor
+public class Application implements CommandLineRunner, ExitCodeGenerator {
 
+	private final CommandLine.IFactory factory;
+	private final ParentCli parentCli;
+	private int exitCode = ErrorCodes.UNKNOWN;
+
+	/**
+	 * Entry point for the application.
+	 *
+	 * @param args arguments for the application.
+	 */
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		final int springExitCode = SpringApplication.exit(
+			SpringApplication.run(Application.class, args)
+		);
+		System.exit(springExitCode);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		exitCode = new CommandLine(parentCli, factory).execute(args);
+	}
+
+	@Override
+	public int getExitCode() {
+		return exitCode;
 	}
 }
