9a0b315aea13892c5c4c0c790a908ad178674ec5
magicDGS <daniel.gomez.sanchez@hotmail.es>
2023-12-19 21:53:36 +0100

    Fix ID common properties

--- model/src/main/java/io/github/ygojson/model/data/properties/CommonProperties.java
+++ model/src/main/java/io/github/ygojson/model/data/properties/CommonProperties.java
@@ -11,7 +11,22 @@ public class CommonProperties {
 	}
 
 	/**
-	 * YGOJSON ID property.
+	 * YGOJSON ID property for the actual model.
 	 */
-	public static final String ID = "id";
+	public static final String SELF_ID = "id";
+
+	/**
+	 * YGOJSON ID property for the linked card model.
+	 */
+	public static final String CARD_ID = "cardId";
+
+	/**
+	 * YGOJSON ID property for the linked print model.
+	 */
+	public static final String PRINT_ID = "printId";
+
+	/**
+	 * YGOJSON ID property for the linked set model.
+	 */
+	public static final String SET_ID = "setId";
 }
