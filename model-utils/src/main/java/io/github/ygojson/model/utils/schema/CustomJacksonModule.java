4b1dfafb3bd6f6c426b5bc9726271af2693a3a91
magicDGS <daniel.gomez.sanchez@hotmail.es>
2023-12-19 20:52:17 +0100

    Add model-utils module with json-schema generation

---
+++ model-utils/src/main/java/io/github/ygojson/model/utils/schema/CustomJacksonModule.java
@@ -0,0 +1,38 @@
+package io.github.ygojson.model.utils.schema;
+
+import com.github.victools.jsonschema.generator.MemberScope;
+import com.github.victools.jsonschema.generator.TypeScope;
+import com.github.victools.jsonschema.module.jackson.JacksonModule;
+import com.github.victools.jsonschema.module.jackson.JacksonOption;
+
+class CustomJacksonModule extends JacksonModule {
+
+	private boolean addDescription = true;
+
+	CustomJacksonModule(final boolean addDescription) {
+		// always include these properties
+		super(
+			JacksonOption.RESPECT_JSONPROPERTY_ORDER,
+			JacksonOption.INCLUDE_ONLY_JSONPROPERTY_ANNOTATED_METHODS,
+			JacksonOption.RESPECT_JSONPROPERTY_REQUIRED,
+			JacksonOption.FLATTENED_ENUMS_FROM_JSONPROPERTY
+		);
+		this.addDescription = addDescription;
+	}
+
+	@Override
+	protected String resolveDescription(MemberScope<?, ?> member) {
+		if (addDescription) {
+			return super.resolveDescription(member);
+		}
+		return null;
+	}
+
+	@Override
+	protected String resolveDescriptionForType(TypeScope type) {
+		if (addDescription) {
+			return super.resolveDescriptionForType(type);
+		}
+		return null;
+	}
+}
--- model/src/test/java/io/github/ygojson/model/data/acceptance/PackageSettings.java
+++
@@ -1,9 +0,0 @@
-package io.github.ygojson.model.data.acceptance;
-
-/**
- * Settings for the approval tests configuration.
- */
-public class PackageSettings {
-
-	public static String ApprovalBaseDirectory = "../resources";
-}
