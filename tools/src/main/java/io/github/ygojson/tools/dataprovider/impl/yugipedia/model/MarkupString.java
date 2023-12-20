48a92ec9e7eb1862cb14c3c36de5846abd7e5bee
magicDGS <daniel.gomez.sanchez@hotmail.es>
2023-12-20 23:30:39 +0100

    Add MarkupString and tests

---
+++ tools/src/main/java/io/github/ygojson/tools/dataprovider/impl/yugipedia/model/MarkupString.java
@@ -0,0 +1,248 @@
+package io.github.ygojson.tools.dataprovider.impl.yugipedia.model;
+
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+/**
+ * String that might contain markup from the Yugipedia API
+ * that can be clenaup.
+ */
+public final class MarkupString {
+
+	/**
+	 * Creates a new instance from the given string.
+	 *
+	 * @param value string value with possible markup.
+	 *
+	 * @return new instance.
+	 */
+	public static MarkupString of(final String value) {
+		return new MarkupString(value, false);
+	}
+
+	/**
+	 * Creates a new instance from the given string that might
+	 * contain some japanese-specific markup too.
+	 *
+	 * @param value string value with possible markup.
+	 *
+	 * @return new instance (japanese-aware).
+	 */
+	public static MarkupString ofJapanese(final String value) {
+		return new MarkupString(value, true);
+	}
+
+	/**
+	 * New line pattern.
+	 */
+	private static final Pattern NEW_LINE_PATTERN = Pattern.compile("\n");
+
+	/**
+	 * Comma separator pattern.
+	 */
+	private static final Pattern COMMA_SEPARATOR_PATTERN = Pattern.compile(", ");
+
+	/**
+	 * Semicolon separator pattern.
+	 */
+	private static final Pattern SEMICOLON_SEPARATOR_PATTERN = Pattern.compile(
+		"; "
+	);
+
+	/**
+	 * Line break markup pattern used along the wikitext.
+	 */
+	private static final Pattern MARKUP_BREAK_PATTERN = Pattern.compile("<br />");
+
+	/**
+	 * Internal link markup pattern used along the wikitext.
+	 */
+	private static final Pattern MARKUP_INTERNAL_LINK_REGEX = Pattern.compile(
+		"\\[\\[(?:[^|\\]]+\\|)?([^\\]]+)\\]\\]"
+	);
+
+	/**
+	 * Internal link markup replacement to be used with {@link #MARKUP_INTERNAL_LINK_REGEX}
+	 */
+	private static final String MARKUP_INTERNAL_LINK_REPLACEMENT = "$1";
+
+	/**
+	 * Italic markup pattern used along the wikitext.
+	 */
+	private static final Pattern MARKUP_ITALIC_REGEX = Pattern.compile(
+		"''(.*?)''"
+	);
+
+	/**
+	 * Italic markup replacement to be used with {@link #MARKUP_ITALIC_REGEX}
+	 */
+	private static final String MARKUP_ITALIC_REPLACEMENT = "$1";
+
+	/**
+	 * Pattern for japanese ruby syntax on the Yugipedia markup format.
+	 */
+	private static final Pattern MARKUP_RUBY_REGEX = Pattern.compile(
+		"\\{\\{Ruby\\|(.*?)\\|(.*?)}}"
+	);
+
+	/**
+	 * Match group for the ruby kanji part of the {@link #MARKUP_RUBY_REGEX}.
+	 */
+	private static final int MARKUP_RUBY_MATCH_GROUP_KANJI = 1;
+	/**
+	 * Match group for the ruby furigana part of the {@link #MARKUP_RUBY_REGEX}.
+	 */
+	private static final int MARKUP_RUBY_MATCH_GROUP_FURIGANA = 2;
+
+	/**
+	 * Message format string for the HTML forma of japanese ruby.
+	 * <br>
+	 * The firs argument is the ruby kanji part and the second is the ruby furigana part.
+	 */
+	private static final String HTML_RUBY_MESSAGE_FORMAT =
+		"<ruby>{0}<rt>{1}</rt></ruby>";
+
+	private final String value;
+	private final boolean expectedJapanese;
+
+	private MarkupString(final String value, final boolean expectedJapanese) {
+		this.value = value;
+		this.expectedJapanese = expectedJapanese;
+	}
+
+	private Stream<MarkupString> splitByPattern(final Pattern pattern) {
+		if (value == null) {
+			return Stream.empty();
+		}
+		return pattern
+			.splitAsStream(value)
+			.map(value -> new MarkupString(value, this.expectedJapanese));
+	}
+
+	/**
+	 * Splits the current markup string by new line.
+	 *
+	 * @param skipBlanks if {@code true}, skip blank lines;
+	 *                   otherwise, keep them on the returned value.
+	 * @return a list of the values separated by new lines.
+	 */
+	public Stream<MarkupString> splitByNewLine(final boolean skipBlanks) {
+		final Stream<MarkupString> stream = splitByPattern(NEW_LINE_PATTERN);
+
+		return skipBlanks ? stream.filter(ms -> !ms.value.isBlank()) : stream;
+	}
+
+	/**
+	 * Splits the current markup string by comma.
+	 *
+	 * @return a list with the comma-separated values.
+	 */
+	public Stream<MarkupString> splitByComma() {
+		return splitByPattern(COMMA_SEPARATOR_PATTERN);
+	}
+
+	/**
+	 * Splits the current markup string by semicolon.
+	 *
+	 * @return a list with the semicolon-separated values.
+	 */
+	public Stream<MarkupString> splitBySemicolon() {
+		return splitByPattern(SEMICOLON_SEPARATOR_PATTERN);
+	}
+
+	/**
+	 * Splits the current markup string by breaks.
+	 * <br>
+	 * Note: empty lines are always removed.
+	 *
+	 * @param maxItems the maximum items to return
+	 *                 (last one might contain breaks still if there are more).
+	 * @return a list with the break-separated values.
+	 */
+	public List<MarkupString> splitByBreak(final int maxItems) {
+		if (value == null) {
+			return List.of();
+		}
+		return Arrays
+			.stream(MARKUP_BREAK_PATTERN.split(value, maxItems))
+			.filter(item -> !item.isEmpty()) // remove empty lines
+			.map(value -> new MarkupString(value, this.expectedJapanese))
+			.toList();
+	}
+
+	/**
+	 * Returns as a string where all the markup have been removed.
+	 * <br>
+	 * Note that for japanese, this might not be true, as the ruby-encoding
+	 * would be returned in the html-form.
+	 *
+	 * @return the string without (or with minimum required) markup.
+	 */
+	public String withoutMarkup() {
+		if (value == null || value.isEmpty()) {
+			return value;
+		}
+		String cleanup = value;
+		if (expectedJapanese) {
+			cleanup = replaceJapaneseRubyByTags(cleanup);
+		}
+		// first remove internal links
+		cleanup =
+			MARKUP_INTERNAL_LINK_REGEX
+				.matcher(cleanup)
+				.replaceAll(MARKUP_INTERNAL_LINK_REPLACEMENT);
+		// then remove italic
+		cleanup =
+			MARKUP_ITALIC_REGEX
+				.matcher(cleanup)
+				.replaceAll(MARKUP_ITALIC_REPLACEMENT);
+
+		// finally remove line-breaks
+		return MARKUP_BREAK_PATTERN.matcher(cleanup).replaceAll("\n");
+	}
+
+	private String replaceJapaneseRubyByTags(final String text) {
+		final Matcher matcher = MARKUP_RUBY_REGEX.matcher(text);
+		final StringBuilder result = new StringBuilder();
+		while (matcher.find()) {
+			final String kanji = matcher.group(MARKUP_RUBY_MATCH_GROUP_KANJI);
+			final String furigana = matcher.group(MARKUP_RUBY_MATCH_GROUP_FURIGANA);
+			matcher.appendReplacement(
+				result,
+				MessageFormat.format(HTML_RUBY_MESSAGE_FORMAT, kanji, furigana)
+			);
+		}
+		matcher.appendTail(result);
+		return result.isEmpty() ? text : result.toString();
+	}
+
+	/**
+	 * Returns the string as-is (including the markup).
+	 *
+	 * @return the string as-is.
+	 */
+	@Override
+	public String toString() {
+		return value;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof MarkupString that)) return false;
+		return (
+			expectedJapanese == that.expectedJapanese &&
+			Objects.equals(value, that.value)
+		);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(value, expectedJapanese);
+	}
+}
---
+++ tools/src/test/java/io/github/ygojson/tools/dataprovider/impl/yugipedia/model/MarkupStringUnitTest.java
@@ -0,0 +1,244 @@
+package io.github.ygojson.tools.dataprovider.impl.yugipedia.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class MarkupStringUnitTest {
+
+	static Stream<Arguments> consideredMarkups() {
+		return Stream.of(
+			Arguments.of(null, null),
+			Arguments.of("", ""),
+			Arguments.of("No markup", "No markup"),
+			Arguments.of("Breaks:<br />first<br />second", "Breaks:\nfirst\nsecond"),
+			Arguments.of("[[Link]]", "Link"),
+			Arguments.of("Before[[Link]]", "BeforeLink"),
+			Arguments.of("[[Link]]After", "LinkAfter"),
+			Arguments.of("[[Link]]Middle[[Link]]", "LinkMiddleLink"),
+			Arguments.of("[[Alt|Link]]", "Link"),
+			Arguments.of("Before[[Alt|Link]]", "BeforeLink"),
+			Arguments.of("[[Alt|Link]]After", "LinkAfter"),
+			Arguments.of("[[Alt|Link]]Middle[[Alt|Link]]", "LinkMiddleLink"),
+			Arguments.of("''Italic''", "Italic"),
+			Arguments.of("Before''Italic''", "BeforeItalic"),
+			Arguments.of("''Italic''After", "ItalicAfter"),
+			Arguments.of("''Italic''Middle''Italic''", "ItalicMiddleItalic")
+		);
+	}
+
+	@ParameterizedTest
+	@MethodSource("consideredMarkups")
+	void given_consideredMarkupfromString_when_withoutMarkup_then_correctlyCleanup(
+		final String maybeJapanese,
+		final String expected
+	) {
+		// given
+		final MarkupString markupString = MarkupString.of(maybeJapanese);
+		// when
+		final String withoutMarkup = markupString.withoutMarkup();
+		// then
+		assertThat(withoutMarkup).isEqualTo(expected);
+	}
+
+	@ParameterizedTest
+	@MethodSource("consideredMarkups")
+	void given_consideredMarkupfromJapaneseString_when_withoutMarkup_then_correctlyCleanup(
+		final String maybeJapanese,
+		final String expected
+	) {
+		// given
+		final MarkupString markupString = MarkupString.ofJapanese(maybeJapanese);
+		// when
+		final String withoutMarkup = markupString.withoutMarkup();
+		// then
+		assertThat(withoutMarkup).isEqualTo(expected);
+	}
+
+	static Stream<Arguments> rubyMarkupProvider() {
+		return Stream.of(
+			Arguments.of(null, null),
+			Arguments.of("", ""),
+			Arguments.of("No markup", "No markup"), // exactly the same
+			Arguments.of("{{Ruby|創|そう}}", "<ruby>創<rt>そう</rt></ruby>"),
+			Arguments.of(
+				"Before{{Ruby|創|そう}}",
+				"Before<ruby>創<rt>そう</rt></ruby>"
+			),
+			Arguments.of(
+				"{{Ruby|創|そう}}After",
+				"<ruby>創<rt>そう</rt></ruby>After"
+			),
+			Arguments.of(
+				"{{Ruby|創|そう}}Middle{{Ruby|創|そう}}",
+				"<ruby>創<rt>そう</rt></ruby>Middle<ruby>創<rt>そう</rt></ruby>"
+			),
+			Arguments.of( // real case
+				"{{Ruby|創|そう}}{{Ruby|聖|せい}}{{Ruby|魔|ま}}{{Ruby|導|どう}}{{Ruby|王|おう}} エンディミオン",
+				"<ruby>創<rt>そう</rt></ruby><ruby>聖<rt>せい</rt></ruby><ruby>魔<rt>ま</rt></ruby><ruby>導<rt>どう</rt></ruby><ruby>王<rt>おう</rt></ruby> エンディミオン"
+			)
+		);
+	}
+
+	@ParameterizedTest
+	@MethodSource("rubyMarkupProvider")
+	void given_fromJapaneseString_when_withoutMarkup_then_correctlyCleanup(
+		final String maybeJapanese,
+		final String expected
+	) {
+		// given
+		final MarkupString markupString = MarkupString.ofJapanese(maybeJapanese);
+		// when
+		final String withoutMarkup = markupString.withoutMarkup();
+		// then
+		assertThat(withoutMarkup).isEqualTo(expected);
+	}
+
+	@ParameterizedTest
+	@MethodSource("rubyMarkupProvider")
+	void given_wrongfromStringForJapanese_when_withoutMarkup_then_sameString(
+		final String maybeJapanese,
+		final String expected
+	) {
+		// given
+		final MarkupString markupString = MarkupString.of(maybeJapanese);
+		// when
+		final String withoutMarkup = markupString.withoutMarkup();
+		// then
+		assertThat(withoutMarkup).isEqualTo(maybeJapanese);
+	}
+
+	@Test
+	void given_fromJapaneseStringWithRubyInsideMarkup_when_withoutMarkup_then_cleanupAndWithRubyTags() {
+		// given
+		final MarkupString markupString = MarkupString.ofJapanese(
+			"[[{{Ruby|kanji1|furigana1}}]]<br />''{{Ruby|kanji2|furigana2}}''"
+		);
+		// when
+		final String withoutMarkup = markupString.withoutMarkup();
+		// then
+		assertThat(withoutMarkup)
+			.isEqualTo(
+				"<ruby>kanji1<rt>furigana1</rt></ruby>\n<ruby>kanji2<rt>furigana2</rt></ruby>"
+			);
+	}
+
+	static Stream<Arguments> newLinesProvider() {
+		return Stream.of(
+			Arguments.of(null, false, List.of()),
+			Arguments.of("", false, List.of("")),
+			Arguments.of("", true, List.of()),
+			Arguments.of("first second", false, List.of("first second")),
+			Arguments.of("first\n", false, List.of("first")),
+			Arguments.of("first\n", true, List.of("first")),
+			Arguments.of("first\nsecond", false, List.of("first", "second")),
+			Arguments.of("first\nsecond", true, List.of("first", "second")),
+			Arguments.of(
+				"first\nsecond\nthird",
+				false,
+				List.of("first", "second", "third")
+			),
+			Arguments.of("first\n\nthird", false, List.of("first", "", "third")),
+			Arguments.of("first\n\nthird", true, List.of("first", "third"))
+		);
+	}
+
+	@ParameterizedTest
+	@MethodSource("newLinesProvider")
+	void given_newLineSeparator_when_splitByNewLine_then_correctSplit(
+		final String string,
+		final boolean skipBlanks,
+		final List<String> expectedResult
+	) {
+		// given
+		final MarkupString markupString = MarkupString.of(string);
+		// when
+		final List<MarkupString> split = markupString
+			.splitByNewLine(skipBlanks)
+			.toList();
+		// then
+		final List<MarkupString> expected = expectedResult
+			.stream()
+			.map(MarkupString::of)
+			.toList();
+		assertThat(split)
+			.describedAs("size")
+			.hasSameSizeAs(expected)
+			.describedAs("contents")
+			.isEqualTo(expected);
+	}
+
+	static Stream<Arguments> breakLinesProvider() {
+		return Stream.of(
+			Arguments.of(null, 3, List.of()),
+			Arguments.of("", 3, List.of()),
+			Arguments.of("first second", 3, List.of("first second")),
+			Arguments.of("first<br />second", 1, List.of("first<br />second")),
+			Arguments.of("first<br />second", 2, List.of("first", "second")),
+			Arguments.of("first<br />second", 3, List.of("first", "second")),
+			Arguments.of(
+				"first<br />second<br />third",
+				2,
+				List.of("first", "second<br />third")
+			),
+			Arguments.of("first<br /><br />", 5, List.of("first"))
+		);
+	}
+
+	@ParameterizedTest
+	@MethodSource("breakLinesProvider")
+	void given_stringWithBreaks_when_split_then_correctSplit(
+		final String string,
+		final int maxItems,
+		final List<String> expectedResult
+	) {
+		// given
+		final MarkupString markupString = MarkupString.of(string);
+		// when
+		final List<MarkupString> split = markupString.splitByBreak(maxItems);
+		// then
+		final List<MarkupString> expected = expectedResult
+			.stream()
+			.map(MarkupString::of)
+			.toList();
+		assertThat(split)
+			.describedAs("size")
+			.hasSameSizeAs(expected)
+			.describedAs("contents")
+			.isEqualTo(expected);
+	}
+
+	@Test
+	void given_stringWithComman_when_splitByComma_then_correctSplit() {
+		// given
+		final MarkupString markupString = MarkupString.of("first, second");
+		// when
+		final List<MarkupString> split = markupString.splitByComma().toList();
+		// then
+		final List<MarkupString> expected = List.of(
+			MarkupString.of("first"),
+			MarkupString.of("second")
+		);
+		assertThat(split).isEqualTo(expected);
+	}
+
+	@Test
+	void given_stringWithSemicolon_when_splitBySemicolon_then_correctSplit() {
+		// given
+		final MarkupString markupString = MarkupString.of("first; second");
+		// when
+		final List<MarkupString> split = markupString.splitBySemicolon().toList();
+		// then
+		final List<MarkupString> expected = List.of(
+			MarkupString.of("first"),
+			MarkupString.of("second")
+		);
+		assertThat(split).isEqualTo(expected);
+	}
+}
