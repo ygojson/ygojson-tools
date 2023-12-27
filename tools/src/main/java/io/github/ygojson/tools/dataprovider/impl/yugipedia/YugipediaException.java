26bf098859a2205f6cd0c546fd09d779239d8420
magicDGS <daniel.gomez.sanchez@hotmail.es>
2023-12-27 07:18:49 +0100

    Add YugipediaException

---
+++ tools/src/main/java/io/github/ygojson/tools/dataprovider/impl/yugipedia/YugipediaException.java
@@ -0,0 +1,14 @@
+package io.github.ygojson.tools.dataprovider.impl.yugipedia;
+
+import io.github.ygojson.tools.common.YgoJsonToolException;
+
+public class YugipediaException extends YgoJsonToolException {
+
+	public YugipediaException(final String message) {
+		super(message);
+	}
+
+	public YugipediaException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+}
