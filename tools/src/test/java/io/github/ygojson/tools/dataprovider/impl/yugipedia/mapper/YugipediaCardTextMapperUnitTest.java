a257d703b584b8dcd3b6af2118796f8707627ca3
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2024-01-05 20:50:23 +0100

    Implement YugipediaCardTextMapperUnitTests to handle edge cases

---
+++ tools/src/test/java/io/github/ygojson/tools/dataprovider/impl/yugipedia/mapper/YugipediaCardTextMapperUnitTest.java
@@ -0,0 +1,64 @@
+package io.github.ygojson.tools.dataprovider.impl.yugipedia.mapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+import io.github.ygojson.model.data.Card;
+import io.github.ygojson.model.data.definition.CardText;
+import io.github.ygojson.tools.dataprovider.impl.yugipedia.model.CardTable2;
+import io.github.ygojson.tools.dataprovider.test.CardTable2Mother;
+
+class YugipediaCardTextMapperUnitTest {
+
+	private static final YugipediaCardTextMapper MAPPER = Mappers.getMapper(
+		YugipediaCardTextMapper.class
+	);
+
+	@Test
+	void given_nullNameAndPendulumEffect_when_mapToMainCardText_then_returnNull() {
+		// given
+		final CardTable2 cardTable2 = CardTable2Mother.withMainCardText(
+			null,
+			null,
+			"Lore Ipsum"
+		);
+		// when
+		final CardText result = MAPPER.mapToMainCardText(cardTable2);
+		// then
+		assertThat(result).isNull();
+	}
+
+	@Test
+	void given_nullLore_when_updateCardTextOnEmptyCard_then_cardTextIsNull() {
+		// given
+		final CardTable2 cardTable2 = CardTable2Mother.withMainCardText(
+			null,
+			null,
+			null
+		);
+		final Card emptyCard = new Card();
+		// when
+		MAPPER.updateCardText(cardTable2, emptyCard);
+		// then
+		assertThat(emptyCard.getCardText()).isNull();
+	}
+
+	@Test
+	void given_Lore_when_updateCardTextOnEmptyCard_then_cardTextIsPresentWithLore() {
+		// given
+		final CardTable2 cardTable2 = CardTable2Mother.withMainCardText(
+			null,
+			null,
+			"Lore Ipsum"
+		);
+		final Card emptyCard = new Card();
+		// when
+		MAPPER.updateCardText(cardTable2, emptyCard);
+		// then
+		final CardText expected = new CardText();
+		expected.setEffectText("Lore Ipsum");
+		assertThat(emptyCard.getCardText()).isEqualTo(expected);
+	}
+}
