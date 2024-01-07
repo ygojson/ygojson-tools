7a22c0ec229147252a592ceca8821724d54e1e2e
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2024-01-07 22:01:02 +0100

    Move CardTable2Mapper acceptance tests and improve approved folder structure

---
+++ tools/src/test/java/io/github/ygojson/tools/dataprovider/impl/yugipedia/acceptance/PackageSettings.java
@@ -0,0 +1,9 @@
+package io.github.ygojson.tools.dataprovider.impl.yugipedia.acceptance;
+
+/**
+ * Settings for the approval tests configuration.
+ */
+public class PackageSettings {
+
+	public static String ApprovalBaseDirectory = "../resources";
+}
