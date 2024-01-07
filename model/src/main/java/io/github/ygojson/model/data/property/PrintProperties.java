a88d57940acaadf0194700ebfd50e1d990b9a34b
Daniel Gómez-Sánchez <7352559+magicDGS@users.noreply.github.com>
2024-01-07 12:11:38 +0100

    Simplify Print to speed up development (#54)

--- model/src/main/java/io/github/ygojson/model/data/property/PrintProperties.java
+++ model/src/main/java/io/github/ygojson/model/data/property/PrintProperties.java
@@ -2,8 +2,8 @@ package io.github.ygojson.model.data.property;
 
 public class PrintProperties {
 
-	public static final String SET_PREFIX = "setPrefix";
-	public static final String PRINT_NUMBER = "printNumber";
+	public static final String PRINT_CODE = "printCode";
+	public static final String FIRST_SERIES_SET = "firstSeriesSet";
 	public static final String RARITY = "rarity";
 
 	private PrintProperties() {
