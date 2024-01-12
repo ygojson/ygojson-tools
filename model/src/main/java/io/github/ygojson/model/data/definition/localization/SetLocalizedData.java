49ffb4f74749aa230e1c73e37ebccb36c9d1380b
Daniel Gómez-Sánchez <7352559+magicDGS@users.noreply.github.com>
2024-01-12 16:00:09 +0100

    Improve set model (#72)
    - Add SetInfo to contain extra data that will  be populated from InfoboxSet template
    - Remove SetText model as it is not any longer only containing localized text (but also other properties)
    - Misc.: Add missing .gitignore for acceptance data

--- model/src/main/java/io/github/ygojson/model/data/definition/localization/SetLocalizedData.java
+++ model/src/main/java/io/github/ygojson/model/data/definition/localization/SetLocalizedData.java
@@ -2,7 +2,7 @@ package io.github.ygojson.model.data.definition.localization;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-import io.github.ygojson.model.data.definition.SetText;
+import io.github.ygojson.model.data.definition.SetInfo;
 
 @JsonPropertyOrder(alphabetic = true)
-public class SetLocalizedData extends AbstractLocalizedData<SetText> {}
+public class SetLocalizedData extends AbstractLocalizedData<SetInfo> {}
--- model/src/main/java/io/github/ygojson/model/data/property/SetProperties.java
+++ model/src/main/java/io/github/ygojson/model/data/property/SetProperties.java
@@ -5,9 +5,10 @@ package io.github.ygojson.model.data.property;
  */
 public class SetProperties {
 
-	public static final String PREFIX = "prefix";
 	public static final String NAME = "name";
-
+	public static final String NAME_ALT = "nameAlt";
+	public static final String SET_CODE = "setCode";
+	public static final String SET_CODE_ALT = "setCodeAlt";
 	public static final String TYPE = "type";
 	public static final String SERIES = "series";
 
