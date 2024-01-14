efff3901b71466d8d6688fb981a17f822aa648d4
Daniel Gómez-Sánchez <7352559+magicDGS@users.noreply.github.com>
2024-01-14 15:18:44 +0100

    Remove Set (and definitions) in favor of schema file (#88)
    * Remove Set (and definitions) in favor of schema file
    * Fix test compilation and expectations

--- model/src/main/java/io/github/ygojson/model/data/definition/localization/SetLocalizedData.java
+++
@@ -1,8 +0,0 @@
-package io.github.ygojson.model.data.definition.localization;
-
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
-import io.github.ygojson.model.data.definition.SetInfo;
-
-@JsonPropertyOrder(alphabetic = true)
-public class SetLocalizedData extends AbstractLocalizedData<SetInfo> {}
