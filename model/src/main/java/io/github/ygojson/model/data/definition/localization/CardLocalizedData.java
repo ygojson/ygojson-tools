97fc6e279a1fa3157d3361465577530f343ce51e
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2024-01-14 02:34:18 +0100

    Remove Card (and definitions) in favor of schema file

--- model/src/main/java/io/github/ygojson/model/data/definition/localization/CardLocalizedData.java
+++
@@ -1,8 +0,0 @@
-package io.github.ygojson.model.data.definition.localization;
-
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
-import io.github.ygojson.model.data.definition.CardText;
-
-@JsonPropertyOrder(alphabetic = true)
-public class CardLocalizedData extends AbstractLocalizedData<CardText> {}
