d990b22643738d949c5ae7e40f58fb31445999d5
Daniel Gómez-Sánchez <7352559+magicDGS@users.noreply.github.com>
2024-01-14 02:00:53 +0100

    Add more serialization tests (#75)

--- model-utils/src/test/java/io/github/ygojson/model/utils/data/CardSerializationUnitTest.java
+++ model-utils/src/test/java/io/github/ygojson/model/utils/data/CardSerializationUnitTest.java
@@ -3,6 +3,8 @@ package io.github.ygojson.model.utils.data;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.instancio.Select.all;
 
+import java.util.List;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,14 @@ import io.github.ygojson.model.data.definition.CardText;
 import io.github.ygojson.model.utils.JsonUtils;
 
 public class CardSerializationUnitTest {
+
+	private static final List<String> FIELDS_TO_EXCLUDE = List.of(
+		"name",
+		"pendulumEffect",
+		"effectText",
+		"flavorText",
+		"materials"
+	);
 
 	@Test
 	void given_cardWithNullCardText_when_serialize_then_textNotPresent()
@@ -24,13 +34,21 @@ public class CardSerializationUnitTest {
 		// when
 		final String value = JsonUtils.getObjectMapper().writeValueAsString(card);
 		// then
-		assertThat(value)
-			.doesNotContain(
-				"name",
-				"pendulumEffect",
-				"effectText",
-				"flavorText",
-				"materials"
-			);
+		assertThat(value).doesNotContain(FIELDS_TO_EXCLUDE);
+	}
+
+	@Test
+	void given_cardWithNonNullMainCardText_when_serialize_then_textPresent()
+		throws JsonProcessingException {
+		// given
+		final Card card = Instancio
+			.of(Card.class)
+			.ignore(all(CardText.class))
+			.create();
+		card.setCardText(Instancio.create(CardText.class));
+		// when
+		final String value = JsonUtils.getObjectMapper().writeValueAsString(card);
+		// then
+		assertThat(value).contains(FIELDS_TO_EXCLUDE);
 	}
 }
