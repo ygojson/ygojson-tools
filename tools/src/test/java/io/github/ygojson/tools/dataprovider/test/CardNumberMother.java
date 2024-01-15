7a5cb8f5454596d5a4f48d5cac6ae7643fde617e
Daniel Gómez-Sánchez <7352559+magicDGS@users.noreply.github.com>
2024-01-15 18:51:55 +0100

    Add CardNumber model and mapper (#91)
    * Add CardNumber model
    * Add CardNumber mapper
    * Add unit-tests

---
+++ tools/src/test/java/io/github/ygojson/tools/dataprovider/test/CardNumberMother.java
@@ -0,0 +1,79 @@
+package io.github.ygojson.tools.dataprovider.test;
+
+import io.github.ygojson.model.data.definition.localization.Region;
+import io.github.ygojson.tools.dataprovider.impl.yugipedia.model.CardNumber;
+
+/**
+ * Mother for {@link CardNumber} test data.
+ */
+public class CardNumberMother {
+
+	private CardNumberMother() {
+		// cannot be instantiated - mother class
+	}
+
+	public static CardNumber ofPrefix(
+		final String stringValue,
+		final String setCode,
+		final Region regionCode,
+		final String printNumberPrefix
+	) {
+		return new CardNumber(
+			stringValue,
+			setCode,
+			regionCode,
+			printNumberPrefix,
+			null,
+			null
+		);
+	}
+
+	public static CardNumber ofPrintNumberOnly(
+		final String stringValue,
+		final String setCode,
+		final Region regionCode,
+		final Integer printNumber
+	) {
+		return new CardNumber(
+			stringValue,
+			setCode,
+			regionCode,
+			null,
+			printNumber,
+			null
+		);
+	}
+
+	public static CardNumber ofSpecialEdition(
+		final String stringValue,
+		final String setCode,
+		final Region regionCode,
+		final String specialEditionCode,
+		final Integer printNumber
+	) {
+		return new CardNumber(
+			stringValue,
+			setCode,
+			regionCode,
+			specialEditionCode,
+			printNumber,
+			null
+		);
+	}
+
+	public static CardNumber ofKSeries(
+		final String stringValue,
+		final String setCode,
+		final Region regionCode,
+		final Integer printNumber
+	) {
+		return new CardNumber(
+			stringValue,
+			setCode,
+			regionCode,
+			null,
+			printNumber,
+			"K"
+		);
+	}
+}
