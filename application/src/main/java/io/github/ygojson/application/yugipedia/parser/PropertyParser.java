7deee4bf4d278183786be4e940122f7b720aca42
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2024-04-26 01:57:21 +0200

    Support Sets properties

--- application/src/main/java/io/github/ygojson/application/yugipedia/parser/PropertyParser.java
+++ application/src/main/java/io/github/ygojson/application/yugipedia/parser/PropertyParser.java
@@ -1,8 +1,10 @@
 package io.github.ygojson.application.yugipedia.parser;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Pattern;
 
+import io.github.ygojson.application.yugipedia.parser.model.SetRow;
 import io.github.ygojson.application.yugipedia.parser.model.YugipediaProperty;
 
 class PropertyParser {
@@ -10,6 +12,12 @@ class PropertyParser {
 	private static final Pattern BULLETED_LIST_PATTERN = Pattern.compile("\\*");
 
 	private static final Pattern COMMA_SEPARATOR_PATTERN = Pattern.compile(", ");
+
+	private static final Pattern NEW_LINE_PATTERN = Pattern.compile("\n");
+
+	private static final Pattern SEMICOLON_SEPARATOR_PATTERN = Pattern.compile(
+		"; "
+	);
 
 	/**
 	 * Types for the parsing of the properties.
@@ -34,8 +42,7 @@ class PropertyParser {
 		return switch (type) {
 			case BULLETED_LIST -> parseListProperty(BULLETED_LIST_PATTERN, value);
 			case COMMA_LIST -> parseListProperty(COMMA_SEPARATOR_PATTERN, value);
-			// TODO: parse set rows
-			case SET_ROWS -> parseTextProperty(value);
+			case SET_ROWS -> parseSetRowsProperty(value);
 			// parse as text
 			case TEXT -> parseTextProperty(value);
 		};
@@ -60,5 +67,50 @@ class PropertyParser {
 			.filter(s -> !s.isBlank()) // remove empty
 			.toList();
 		return new YugipediaProperty.ListProp(stringList);
+	}
+
+	private YugipediaProperty parseSetRowsProperty(String value) {
+		final List<SetRow> setRows = NEW_LINE_PATTERN
+			.splitAsStream(value)
+			.filter(line -> !line.isBlank())
+			.map(this::parseSingleSetRow)
+			.filter(Objects::nonNull)
+			.toList();
+		return new YugipediaProperty.SetsProp(setRows);
+	}
+
+	private SetRow parseSingleSetRow(String line) {
+		final String[] split = SEMICOLON_SEPARATOR_PATTERN.split(
+			line,
+			4 // parse one more field to remove extra columsn that might be not expected
+		);
+		return switch (split.length) {
+			case 0 -> null;
+			case 1 -> new SetRow(parseNullableText(split[0]), null, List.of());
+			case 2 -> new SetRow(
+				parseNullableText(split[0]),
+				parseNullableText(split[1]),
+				List.of()
+			);
+			default -> new SetRow(
+				parseNullableText(split[0]),
+				parseNullableText(split[1]),
+				COMMA_SEPARATOR_PATTERN
+					.splitAsStream(split[2])
+					.filter(s -> !s.isBlank())
+					.toList()
+			);
+		};
+	}
+
+	private String parseNullableText(final String value) {
+		if (value == null) {
+			return null;
+		}
+		String trimmed = value.trim();
+		if (trimmed.isEmpty()) {
+			return null;
+		}
+		return trimmed;
 	}
 }
--- application/src/test/java/io/github/ygojson/application/yugipedia/parser/PropertyParserUnitTest.java
+++ application/src/test/java/io/github/ygojson/application/yugipedia/parser/PropertyParserUnitTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import io.github.ygojson.application.yugipedia.parser.model.SetRow;
 import io.github.ygojson.application.yugipedia.parser.model.YugipediaProperty;
 
 class PropertyParserUnitTest {
@@ -104,5 +105,91 @@ class PropertyParserUnitTest {
 		// then
 		assertThat(yugipediaProperty)
 			.isEqualTo(new YugipediaProperty.ListProp(expectedStrings));
+	}
+
+	static Stream<Arguments> setRowsProvider() {
+		return Stream.of(
+			Arguments.of(
+				"Single String",
+				List.of(new SetRow("Single String", null, List.of()))
+			),
+			Arguments.of(
+				"; Vol.1; Ultra Rare",
+				List.of(new SetRow(null, "Vol.1", List.of("Ultra Rare")))
+			),
+			Arguments.of(
+				"Card number 1; Set page name 1; Rarity 1",
+				List.of(
+					new SetRow("Card number 1", "Set page name 1", List.of("Rarity 1"))
+				)
+			),
+			Arguments.of(
+				"Card number 2; Set page name 2; Rarity 2",
+				List.of(
+					new SetRow("Card number 2", "Set page name 2", List.of("Rarity 2"))
+				)
+			),
+			Arguments.of(
+				"CHIM-JP010; Chaos Impact; Super Rare, Secret Rare, 20th Secret Rare",
+				List.of(
+					new SetRow(
+						"CHIM-JP010",
+						"Chaos Impact",
+						List.of("Super Rare", "Secret Rare", "20th Secret Rare")
+					)
+				)
+			),
+			Arguments.of(
+				"????-EN0??; Unknown Set; ",
+				List.of(new SetRow("????-EN0??", "Unknown Set", List.of()))
+			),
+			Arguments.of("; ; ", List.of(new SetRow(null, null, List.of()))),
+			Arguments.of(
+				"CHIM-JP010; Chaos Impact; Super Rare, Secret Rare, 20th Secret Rare",
+				List.of(
+					new SetRow(
+						"CHIM-JP010",
+						"Chaos Impact",
+						List.of("Super Rare", "Secret Rare", "20th Secret Rare")
+					)
+				)
+			),
+			Arguments.of(
+				"; Legend of Blue Eyes White Dragon; Common\nSDK-001; Starter Deck: Kaiba; \nCHIM-EN0??; Chaos Impact; \n????-EN???; Eternity Code; ",
+				List.of(
+					new SetRow(
+						null,
+						"Legend of Blue Eyes White Dragon",
+						List.of("Common")
+					),
+					new SetRow("SDK-001", "Starter Deck: Kaiba", List.of()),
+					new SetRow("CHIM-EN0??", "Chaos Impact", List.of()),
+					new SetRow("????-EN???", "Eternity Code", List.of())
+				)
+			),
+			Arguments.of(
+				"MS-EN001; My set; Rare; New column",
+				List.of(new SetRow("MS-EN001", "My set", List.of("Rare")))
+			)
+		);
+	}
+
+	@ParameterizedTest
+	@MethodSource("setRowsProvider")
+	void given_setRowsString_when_parseSetsProp_then_correctProperty(
+		final String string,
+		final List<SetRow> expected
+	) {
+		// given - string
+		// when
+		final YugipediaProperty actual = PARSER.parse(
+			PropertyParser.Type.SET_ROWS,
+			string
+		);
+		// then
+		final YugipediaProperty expectedProperty = new YugipediaProperty.SetsProp(
+			expected
+		);
+		assertThat(actual).isEqualTo(expectedProperty);
 	}
 }
