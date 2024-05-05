33fcd8deb4642c3bc891e16c5f4bc386dcd24080
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2024-05-06 01:46:29 +0200

    Remove old acceptance tests

--- tools/src/test/java/io/github/ygojson/tools/dataprovider/impl/yugipedia/acceptance/PackageSettings.java
+++
@@ -1,9 +0,0 @@
-package io.github.ygojson.tools.dataprovider.impl.yugipedia.acceptance;
-
-/**
- * Settings for the approval tests configuration.
- */
-public class PackageSettings {
-
-	public static String ApprovalBaseDirectory = "../resources";
-}
