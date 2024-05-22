961b00fcdadc7b444b19d9945570d6b046459261
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2024-05-22 09:16:49 +0200

    Fix build (and remove comment)

--- src/test/java/io/github/ygojson/application/core/db/set/SetRepositoryUnitTest.java
+++ src/test/java/io/github/ygojson/application/core/db/set/SetRepositoryUnitTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.instancio.Select.field;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
@@ -29,7 +30,7 @@ class SetRepositoryUnitTest {
 		// given
 		final SetEntity entity = Instancio
 			.of(SetEntity.class)
-			.ignore(field("id"))
+			.ignore(field(PanacheEntity.class, "id"))
 			.create();
 		// when
 		final Long result = repository.save(entity);
