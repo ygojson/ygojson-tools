40bed9de6dfad400d25f9a87af107964d244e5f3
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2024-06-14 21:16:45 +0200

    Add CardEntity + repository (with flyway SQL-script)

---
+++ src/main/java/io/github/ygojson/application/core/db/converter/JsonStringListAttributeConverter.java
@@ -0,0 +1,40 @@
+package io.github.ygojson.application.core.db.converter;
+
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import org.slf4j.LoggerFactory;
+
+public class JsonStringListAttributeConverter
+	implements AttributeConverter<List<String>, String> {
+
+	private final TypeReference<List<String>> ref = new TypeReference<>() {};
+	private final ObjectMapper mapper = new ObjectMapper();
+
+	@Override
+	public String convertToDatabaseColumn(final List<String> attribute) {
+		try {
+			return mapper.writeValueAsString(attribute);
+		} catch (JsonProcessingException e) {
+			LoggerFactory
+				.getLogger(this.getClass())
+				.warn("Cannot convert list to json", e);
+			return null;
+		}
+	}
+
+	@Override
+	public List<String> convertToEntityAttribute(final String dbData) {
+		try {
+			return mapper.readValue(dbData, ref);
+		} catch (JsonProcessingException e) {
+			LoggerFactory
+				.getLogger(this.getClass())
+				.warn("Cannot convert json to List<String>", e);
+			return null;
+		}
+	}
+}
