e32020786bed17204faacf126b95e24a9de48696
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2024-06-16 20:13:44 +0200

    Add YugipediaWiremockResource and YugipediaMockProfile (not functional as it is missing the mock files

---
+++ src/test/java/io/github/ygojson/application/yugipedia/testutil/YugipediaWiremockResource.java
@@ -0,0 +1,18 @@
+package io.github.ygojson.application.yugipedia.testutil;
+
+import io.github.ygojson.application.testutil.server.WiremockResource;
+
+/**
+ * The Yugipedia {@link WiremockResource}.
+ * <br>
+ * IMPORTANT: this resource should not be used globally with the
+ * {@link io.quarkus.test.common.QuarkusTestResource} annotation,
+ * but rather with the provided {@link YugipediaMockProfile}.
+ */
+public class YugipediaWiremockResource extends WiremockResource {
+
+	@Override
+	public String getName() {
+		return "yugipedia";
+	}
+}
