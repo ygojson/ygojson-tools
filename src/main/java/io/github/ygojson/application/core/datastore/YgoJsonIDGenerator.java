1f5c6bcf1de887fbbc915e4a99b6e0457c74ee15
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2024-09-15 20:29:38 +0200

    Move YGOJSON UUID logic to datastore

--- src/main/java/io/github/ygojson/application/core/datastore/YgoJsonIDGenerator.java
+++ src/main/java/io/github/ygojson/application/core/datastore/YgoJsonIDGenerator.java
@@ -1,4 +1,4 @@
-package io.github.ygojson.application.logic.uuid;
+package io.github.ygojson.application.core.datastore;
 
 import java.util.List;
 import java.util.Objects;
@@ -10,20 +10,12 @@ import java.util.stream.Stream;
 import com.fasterxml.uuid.Generators;
 import com.fasterxml.uuid.impl.NameBasedGenerator;
 import com.fasterxml.uuid.impl.UUIDUtil;
+import jakarta.inject.Singleton;
 
-import io.github.ygojson.model.data.Card;
-import io.github.ygojson.model.data.Print;
-import io.github.ygojson.model.data.Set;
-import io.github.ygojson.model.data.definition.Identifiers;
+import io.github.ygojson.application.core.datastore.db.card.RawCard;
 
-/**
- * Generator for IDs for YGOJSON.
- *
- * @deprecated should use the datastore-database entities instead
- * ({@link io.github.ygojson.application.core.datastore.YgoJsonIDGenerator}).
- */
-@Deprecated
-public class YgojsonIDGenerator {
+@Singleton
+public class YgoJsonIDGenerator {
 
 	private static final NameBasedGenerator NAMESPACE_GENERATOR =
 		Generators.nameBasedGenerator(NameBasedGenerator.NAMESPACE_OID);
@@ -54,7 +46,9 @@ public class YgojsonIDGenerator {
 			throw new IllegalArgumentException(name + " shouldn't be null");
 		}
 		if (idGetter.apply(object) != null) {
-			throw new IllegalArgumentException(name + " shouldn't have an ID");
+			throw new IllegalArgumentException(
+				name + " shouldn't have already a YGOJSON-ID"
+			);
 		}
 	}
 
@@ -66,48 +60,14 @@ public class YgojsonIDGenerator {
 	 * @return the generated ID for a card.
 	 * @throws IllegalArgumentException if the card is {@code null} or the ID is already present.
 	 */
-	public UUID generate(final Card card) {
-		checkRequirements(card, Card::getId, "Card");
+	public UUID generate(final RawCard card) {
+		checkRequirements(card, c -> c.ygojsonId, "Card");
 		return generateInternal(
-			Namespace.CARD,
+			YgoJsonIDGenerator.Namespace.CARD,
 			Stream.of(
-				getOrNull(card.getIdentifiers(), Identifiers::getKonamiId),
-				getOrNull(card.getIdentifiers(), Identifiers::getPassword)
+				getOrNull(card.identifiers, i -> i.konamiId),
+				getOrNull(card.identifiers, i -> i.password)
 			)
-		);
-	}
-
-	/**
-	 * Generates the ID of a set.
-	 *
-	 * @param set object to generate the ID for.
-	 *
-	 * @return the generated ID for a set.
-	 * @throws IllegalArgumentException if the set is {@code null} or the ID is already present.
-	 */
-	public UUID generate(final Set set) {
-		checkRequirements(set, Set::getId, "Set");
-		return generateInternal(
-			Namespace.SET,
-			// FIXME: https://github.com/ygojson/ygojson-tools/issues/113 - should handle OCG/TCG exclusive sets with same name
-			Stream.of(set.getName(), set.getSetCode(), set.getPrintNumberPrefix())
-		);
-	}
-
-	/**
-	 * Generates the ID of a print.
-	 *
-	 * @param print object to generate the ID for.
-	 *
-	 * @return the generated ID for a print.
-	 * @throws IllegalArgumentException if the print is {@code null} or the ID is already present.
-	 */
-	public UUID generate(final Print print) {
-		checkRequirements(print, Print::getId, "Print");
-		return generateInternal(
-			Namespace.PRINT,
-			// FIXME: https://github.com/ygojson/ygojson-tools/issues/112 - should handle problematic prints
-			Stream.of(print.getPrintCode(), print.getRarity())
 		);
 	}
 
@@ -118,7 +78,10 @@ public class YgojsonIDGenerator {
 		return object == null ? null : getter.apply(object);
 	}
 
-	private UUID generateInternal(Namespace namespace, Stream<Object> fields) {
+	private UUID generateInternal(
+		YgoJsonIDGenerator.Namespace namespace,
+		Stream<Object> fields
+	) {
 		final AtomicInteger nullFields = new AtomicInteger(0);
 		final List<String> fieldsAsString = fields
 			.peek(field -> {
